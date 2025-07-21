36bed3cdb24f601402cb1a255a8890529aacbe55
Timothy Hagele <timhagele@icloud.com>
2025-07-21 01:07:18 -0400

    main

--- Sources/Persisted Scroll Position/Persisted_Scroll_Position.swift
+++ Sources/Persisted Scroll Position/Persisted_Scroll_Position.swift
@@ -5,21 +5,19 @@
 
 import SwiftUI 
 
-
+fileprivate let textFieldCount: Int = 10
 
 @available ( iOS 15.0 , macOS 12.0 , * )
 struct Maintained_Scroll_Position_Demo: View {
-	@State var text1: String = ""
-	@State var text2: String = ""
-
+	@State var textItems: Array < String > = Array ( repeating: "" , count: textFieldCount )
 	var body: some View {
-		NavigationView {
-			MaintainedScrollPosition {
-				VStack ( alignment: .leading , spacing: 10 , content: {
-					MultilineTextFieldView2 ( text: self.$text1 , placeholder: "#1" )
-					MultilineTextFieldView2 ( text: self.$text2 , placeholder: "#2" )
-				} )
-				.navigationTitle ( "MaintainedScrollPosition" )
+		MaintainedScrollPosition {
+		ScrollView {
+			VStack ( alignment: .leading , spacing: 10 , content: {
+				ForEach ( 0..<textFieldCount , id:\.self ) { i in
+					MultilineTextFieldView2 ( text: self.$textItems [ i ] , placeholder: "#\( i.description ) TextField" )
+				}
+			} ) .navigationTitle ( "MaintainedScrollPosition" )
 			}
 		}
 	}
@@ -28,15 +26,9 @@ struct Maintained_Scroll_Position_Demo: View {
 #Preview {
 	if #available ( iOS 15.0 , macOS 12.0 , * ) {
 		Maintained_Scroll_Position_Demo ()
-	} else {
-		Text ( "err" )
 	}
+	else { Text ( "err" ) }
 }
-
-
-
-
-
 
 @available ( iOS 15.0 , macOS 12.0 , * )
 fileprivate struct MultilineTextFieldView2: View {
@@ -45,72 +37,58 @@ fileprivate struct MultilineTextFieldView2: View {
 	@FocusState private var isFocused: Bool
 
 	var body: some View {
+		
 		VStack ( alignment: .leading , spacing: 5 ) {
-			HStack {
-				Text ( "Text Field" )
-				Spacer()
-				Text ( "\( self.text.count ) chars" )
-			}
-
-			TextEditor ( text: self.$text )
-				.frame ( minHeight: 35 )
-				.focused ( $isFocused )
-				.overlay (
-					RoundedRectangle ( cornerRadius: 12 )
-						.strokeBorder ( isFocused ? Color.blue : Color.gray , lineWidth: 2 )
-				)
-				.overlay (
-					Text ( text == "" ? placeholder : "" )
-						.foregroundColor ( .secondary )
-						.padding ( .horizontal , 5 )
-						.allowsHitTesting ( true )
-						.frame ( maxWidth: .infinity , alignment: .leading )
-				)
+			HStack { Text ( "Text Field" ) ; Spacer() ; Text ( "\( self.text.count ) chars" ) }
+			
+				TextEditor ( text: self.$text )
+					.frame ( minHeight: 45 )
+					.focused ( $isFocused )
+					.overlay ( RoundedRectangle ( cornerRadius: 12 ) .strokeBorder ( isFocused ? Color.blue : Color.gray , lineWidth: 2 ) )
+					.overlay (
+						Text ( text == "" ? placeholder : "" )
+							.foregroundColor ( .secondary )
+							.padding ( .horizontal , 5 )
+							.allowsHitTesting ( false )
+							.frame ( maxWidth: .infinity , alignment: .leading )
+					 )
 		}
 	}
 }
 
 struct MaintainedScrollPosition < V : View > : View {
-	@State private var frameHeight      : Double = .zero
-	@State private var viewHeight       : Double = .zero
-	@State private var scrollOffset     : Double = .zero
-
-	private var scrollRange             : Double { self.frameHeight - self.viewHeight }
-	private var scrollPercentage        : Double { self.scrollOffset / self.scrollRange }
-
-	var content: V
-
+ @State private var frameHeight   : Double = .zero
+ @State private var viewHeight    : Double = .zero
+ @State private var scrollOffset  : Double = .zero
+ private var scrollRange          : Double { self.frameHeight - self.viewHeight }
+ private var scrollPercentage     : Double { self.scrollOffset / self.scrollRange }
+ private var content: V
 	init ( @ViewBuilder _ content: @escaping () -> V ) { self.content = content() }
-
 	var body: some View {
-		GeometryReader { frameProxy in
-			ScrollView ( .vertical , showsIndicators: true , content: {
-				self.content .padding ( 20 )
-					.background (
-						GeometryReader { viewProxy in
-							ScrollViewReader { scrollProxy in
-								Color.clear .id ( "scrollTo" )
-									
-									.onChange ( of: viewProxy.frame ( in: .named ( "scroll_id" ) ).origin.y ) {
-										self.scrollOffset = $0
-									}
-									.onChange ( of: viewProxy.size.height ) { newViewSize in
-											let deltaY = newViewSize - self.viewHeight
-											let translation = deltaY / ( self.frameHeight - newViewSize )
-											self.viewHeight = newViewSize
-										DispatchQueue.main.async {
-											scrollProxy.scrollTo ( "scrollTo" , anchor: .init ( x: 1 , y: ( scrollPercentage - translation ) ) )
-										}
-									}
-									.onChange ( of: frameProxy.size.height ) {
-										self.frameHeight = $0
-									}
+	 GeometryReader { frameProxy in
+		 ScrollView ( .vertical, showsIndicators: true , content: {
+			 self.content .padding ( 20 )
+				.background (
+					GeometryReader { viewProxy in
+						ScrollViewReader { scrollProxy in
+						Color.clear .id ( "scrollTo" )
+							.onChange ( of: viewProxy.frame ( in: .named ( "scroll_id" ) ).origin.y ) { self.scrollOffset = $0 }
+							.onAppear { self.viewHeight = viewProxy.size.height }
+							.onChange ( of: viewProxy.size.height ) { newViewSize in
+								let deltaY = newViewSize - self.viewHeight
+								let translation = deltaY / ( self.frameHeight - newViewSize )
+								self.viewHeight = newViewSize
+								scrollProxy.scrollTo ( "scrollTo" , anchor: .init ( x: 0  , y: ( scrollPercentage - translation ) ) )
 							}
+							.onAppear { self.frameHeight = frameProxy.size.height }
+							.onChange ( of: frameProxy.size.height ) { self.frameHeight = $0 }
 						}
-					)
-			} )
-			.coordinateSpace ( name: "scroll_id" )
+					}
+				)
+			} ) .coordinateSpace ( name: "scroll_id" )
 		}
 	}
 }
+
+
 
